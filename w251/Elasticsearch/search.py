14d5f8fa9973ca31bd6990e8cdbfd2120ebb21ee
Praba Santhanakrishnan <prabas@Prabas-MBP-166.hsd1.wa.comcast.net>
2018-11-24 19:31:09 -0800

    Crawl and Seach

---
+++ w251/Elasticsearch/search.py
@@ -0,0 +1,22 @@
+import json
+import sys
+import requests
+
+def main():
+    search_term=sys.argv[1]
+    search_url = 'http://169.54.142.59:9200/agri/india'
+    response = requests.get(search_url + '/_search?q=' + search_term + '&pretty')
+    if response.status_code == 200:
+        results = json.loads(response.text)
+        print('\n')
+        for item in results['hits']['hits']:
+            print(item['_source']['title'])
+            print (item['_source']['url'])
+            print('\n')
+            print (item['_source']['description'])
+            print('______________________________________________________________________________________________________________')
+
+    else:
+        print('No results found.')
+
+main()
